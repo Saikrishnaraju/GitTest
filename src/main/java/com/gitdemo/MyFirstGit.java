3de7f51b8b409993e03a25c183da5c74eae33bc9
Saikrishnaraju <54949326+Saikrishnaraju@users.noreply.github.com>
2024-10-11 15:08:17 +0530

    This is master.

---
+++ src/main/java/com/gitdemo/MyFirstGit.java
@@ -0,0 +1,12 @@
+package com.gitdemo;
+
+public class MyFirstGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I am from myfirstgit");
+System.out.println("I am from second");
+System.out.println("I am from third");
+	}
+
+}
